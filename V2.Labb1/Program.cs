78d3425bb4ad672587266a4bc5cc9ed69b3581d5
soisoisoisoisoi <nisser12@outlook.com>
2024-11-25 13:58:38 +0100

    lade till kommentarer

--- V2.Labb1/Program.cs
+++ V2.Labb1/Program.cs
@@ -6,6 +6,9 @@
     {
         static void Main(string[] args)
         {
+
+            //Consolen ger ut ett meddelande med 4st val
+
             Console.WriteLine("****************************************");
             Console.WriteLine("\nVälkommen till flygtidsberäknaren!\n");
             Console.WriteLine("Vilket flyg vill du se detaljerad information om? (svara med siffra)\n");
@@ -14,34 +17,51 @@
             Console.WriteLine("3. Avsluta programmet\n");
             Console.Write("Skriv ditt val här: ");
 
+            //Användaren får trycka på sitt val
+            //Readkey() och KeyChar() låter oss få en input utan att klicka på enter
+
             char inputChar = Console.ReadKey().KeyChar;
             Console.WriteLine();
 
-            // Tider och tidsinställningar
+            // Avgångstider
+
             int stockholmHourDepart = 14;
             int stockholmMinuteDepart = 3;
             int newYorkHourDepart = 10;
             int newYorkMinuteDepart = 10;
 
+            // Restid i timmar och minuter
+
             int travelHours = 7;
             int travelMinutes = 25;
 
-            // Tidskillnad
+            // Tidskillnad i timmar
+
             int timeDifference = 6;
 
+
+            // Switchcase med alla alternativ användaren kan välja
             switch (inputChar)
             {
                 case '1':
                     Console.WriteLine("Du har valt flyget från Stockholm till New York.\n");
                     Console.WriteLine($"Avgångstid för ditt flyg är {stockholmHourDepart:D2}:{stockholmMinuteDepart:D2}\n");
+
                     // Ankomsttid i New York
+                    // Tar avgångstiden från Sthlm och lägger på restid och drar av tidskillnad, först timmar sedan minuter
+
                     int newYorkHourArrival = stockholmHourDepart - timeDifference + travelHours;
                     int newYorkMinuteArrival = stockholmMinuteDepart + travelMinutes;
+
+                    // Begränsar minuter till 60, går den över adderas +1 till timmar
+
                     if (newYorkMinuteArrival >= 60)
                     {
                         newYorkMinuteArrival -= 60;
                         newYorkHourArrival++;
                     }
+                    // Skriver ut ankomst i formatet HH:mm
+
                     Console.WriteLine($"Din ankomsttid i New York är: {newYorkHourArrival:D2}:{newYorkMinuteArrival:D2}");
                     break;
 
@@ -49,6 +69,8 @@
                     Console.WriteLine("Du har valt flyget från New York till Stockholm.\n");
                     Console.WriteLine($"Avgångstid för ditt flyg är {newYorkHourDepart:D2}:{newYorkMinuteDepart:D2}\n");
                     // Ankomsttid i Stockholm
+                    // Tar avgångstiden från NY och lägger på restid+tidsskillnad, först timmar sedan minuter
+
                     int stockholmHourArrival = newYorkHourDepart + timeDifference + travelHours;
                     int stockholmMinuteArrival = newYorkMinuteDepart + travelMinutes;
                     if (stockholmMinuteArrival >= 60)
@@ -59,12 +81,14 @@
                     Console.WriteLine($"Din ankomsttid i Stockholm är: {stockholmHourArrival:D2}:{stockholmMinuteArrival:D2}");
                     break;
 
-                case '3':
+                case '3':       // Trycker användaren på 3 avslutas programmet
+
                     Console.WriteLine("Programmet avslutas.");
                     Environment.Exit(0);
                     break;
 
-                default:
+                default:        // Trycker användaren på något annat än 1-3 får man felmeddelande
+
                     Console.WriteLine("Ogiltigt val.");
                     break;
             }
